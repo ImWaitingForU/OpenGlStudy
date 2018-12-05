753c302db470c65805f8daccb2671d69d2f9ed65
chan <870536460@qq.com>
2018-12-06 00:52:53 +0800

    f cmt

---
+++ Project1/main.cpp
@@ -0,0 +1,29 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include<iostream>
+
+//≥ı ºªØglfw
+void InitGlfw()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+int main()
+{
+
+	InitGlfw();
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	return 0;
+}
