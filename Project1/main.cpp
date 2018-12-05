0913aca7e869c09717c022f0ea772cb852cc38ff
chan <870536460@qq.com>
2018-12-06 01:01:58 +0800

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,7 +2,6 @@
 #include <GLFW/glfw3.h>
 #include<iostream>
 
-//≥ı ºªØglfw
 void InitGlfw()
 {
 	glfwInit();
@@ -11,10 +10,26 @@ void InitGlfw()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
+void InitGlad()
+{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	std::cout << "[framebuffer_size_callback]" + width + "-" + height << std::endl;
+	glViewport(0, 0, width, height);
+}
+
 int main()
 {
 
 	InitGlfw();
+	InitGlad();
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
@@ -24,6 +39,8 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
+	glViewport(0, 0, 800, 600);
 
 	return 0;
 }
