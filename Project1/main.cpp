03d6e4a44389ea400998cd56c1b7eb926c94b57e
Roshan <870536460@qq.com>
2018-12-06 11:47:43 +0800

    run  window success~

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,36 +1,26 @@
+#define GLUT_DISABLE_ATEXIT_HACK
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include<iostream>
+#include <iostream>
 
-void InitGlfw()
-{
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-}
-
-void InitGlad()
-{
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	std::cout << "[framebuffer_size_callback]" + width + "-" + height << std::endl;
-	glViewport(0, 0, width, height);
+	std::cout << "[framebuffer_size_callback]width==>" + width << std::endl;
+	std::cout << "[framebuffer_size_callback]height==>" + height << std::endl;
+	//glViewport(0, 0, width, height);
 }
 
 int main()
 {
+	//InitGlfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	InitGlfw();
-	InitGlad();
-
+	//创建窗口
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -40,7 +30,28 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
-	glViewport(0, 0, 800, 600);
+	//InitGlad,在设置glfwContext后，否则会初始化失败
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
+	//设置窗口大小 左下角位置+宽高(像素)
+	glViewport(0, 0, 800, 600);
+	//当用户改变窗口的大小的时候回调
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	/*
+		glfwWindowShouldClose函数在我们每次循环的开始前检查一次GLFW是否被要求退出，如果是的话该函数返回true然后渲染循环便结束了，之后为我们就可以关闭应用程序了。
+		glfwPollEvents函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）。
+		glfwSwapBuffers函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
+	*/
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
